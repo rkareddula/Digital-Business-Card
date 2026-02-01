f2fc49d8de81e7cfa5a0695328b898860953223a
Roshan Kareddula <99454334+rkareddula@users.noreply.github.com>
2026-02-01 18:34:41 -0500

    Update relative path

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -3,7 +3,7 @@ export default function Info() {
     <header className="info-section">
       <div className="image-container">
         <img
-          src="../src/assets/avatar.jpeg"
+          src="../src/assets/images/avatar.jpeg"
           alt="Profile picture"
           className="profile-img"
         />
